4972c5ce706ffdf3a98416d4869c7141df36bc5d
keiru517 <adrianbotteon@gmail.com>
2023-08-21 22:44:08 +0300

    added google

--- src/auth/google-login/index.js
+++ src/auth/google-login/index.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import { useContext } from 'react';
 
 import { GoogleLogin } from 'react-google-login';
 // refresh token
 import { refreshTokenSetup } from '../utils/refreshToken';
+import { AuthContext } from 'context';
 
 const clientId =
   '707788443358-u05p46nssla3l8tmn58tpo9r5sommgks.apps.googleusercontent.com';
 
 function Login() {
+  const authContext = useContext(AuthContext);
+
   const onSuccess = (res) => {
     console.log('Login Success: currentUser:', res.profileObj);
     alert(
       `Logged in successfully welcome ${res.profileObj.name} 😍. \n See console for full profile object.`
     );
-    refreshTokenSetup(res);
+    // refreshTokenSetup(res);
+    authContext.login("asdf", "asdf");
   };
 
   const onFailure = (res) => {
